f97ec6ecf3d1bc0acd298085f76d43b2bf164b11
panitad <139569094+panitad@users.noreply.github.com>
2024-02-24 23:35:27 -0800

    Create w.py

---
+++ w.py
@@ -0,0 +1,16 @@
+resu1 = ""
+for i in range(1, 16, 2):
+    resu1 += str(i) + ", "
+print(resu1)
+resu2 = ""
+for i in range(2, 20, 3):
+    resu2 += str(i) + ", "
+print(resu2)
+resu3 = ""
+for i in range(30, -31, -10):
+    resu3 += str(i) + ", "
+print(resu3)
+resu4 = ""
+for i in range(15, 56, 8):
+    resu4 += str(i) + ", "
+print(resu4)
